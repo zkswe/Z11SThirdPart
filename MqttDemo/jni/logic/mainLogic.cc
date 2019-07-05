52a936e04f4710a50160a82a07c96e7a375b23cd
guoxs <guoxs@zkswe.com>
2019-07-05 10:15:46 +0800

    mqtt添加连接失败销毁client对象操作

--- MqttDemo/jni/logic/mainLogic.cc
+++ MqttDemo/jni/logic/mainLogic.cc
@@ -68,6 +68,7 @@ protected:
 		// 连接
 		if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
 			LOGD("Failed to connect, return code %d\n", rc);
+			MQTTClient_destroy(&client);
 			return false;
 		}
 
@@ -166,6 +167,7 @@ protected:
 
 		if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
 			LOGD("Failed to connect, return code %d\n", rc);
+			MQTTClient_destroy(&client);
 			return false;
 		}
 
